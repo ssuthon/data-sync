058bd6bb83a9c237be63a016f128a1720ce9b977
Suthon <suthon@gmail.com>
2016-07-10 00:38:48 +0700

    data uplodable

--- grails-app/controllers/box/DsyncController.groovy
+++ grails-app/controllers/box/DsyncController.groovy
@@ -5,7 +5,7 @@ class DsyncController {
 	def grailsApplication
 
 	def forUpdated(String store, Long lastSyncSeq){
-		def dc = findDomainClassByName(store)
+		def dc = findDomainClassByName(store.capitalize())
 		if(!dc){
 			response.sendError(422)
 			return
@@ -14,13 +14,15 @@ class DsyncController {
 		lastSyncSeq = lastSyncSeq ?: 0
 		def result = dc.createCriteria().list {
 			gt 'syncSeq', lastSyncSeq
+			order 'syncSeq', 'asc'
+			maxResults 100
 		}
 
 		render result as JSON
 	}
 
 	def forDeleted(String store, Long lastSyncSeq){
-		def dc = findDomainClassByName(store)
+		def dc = findDomainClassByName(store.capitalize())
 		if(!dc){
 			response.sendError(422)
 			return
@@ -30,8 +32,29 @@ class DsyncController {
 		def result = DeletedEntitySyncLog.createCriteria().list {
 			eq 'store', store
 			gt 'syncSeq', lastSyncSeq
+			order 'syncSeq', 'asc'
+			maxResults 100
 		}
 
+		render result as JSON
+	}
+
+	def upload(String store){
+		def dc = findDomainClassByName(store.capitalize())
+    	if(!dc || !dc.isAnnotationPresent(DataUploadable)){
+			response.sendError(422)
+			return
+		}
+		def instance = dc.newInstance()
+		def jsonData = request.JSON
+		def result = [valid: false]
+		try{
+			bindData(instance, jsonData)
+			instance.save(flush: true)
+			if(instance.hasErrors())
+				log.debug instance.errors
+			result.valid = (instance.id != null)
+		}catch(e){}
 		render result as JSON
 	}
 
---
+++ src/java/box/DataUploadable.java
@@ -0,0 +1,18 @@
+package box;
+
+import org.codehaus.groovy.transform.GroovyASTTransformationClass;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Retention;
+
+/**
+ * Annotation used for GORM domain classes that should have the following
+ * properties syncSeq
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
+public @interface DataUploadable {
+}
